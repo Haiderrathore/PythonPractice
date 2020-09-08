0f16a3c1be71713946442ca12173a6ee293a902b
Haiderrathore <70682547+Haiderrathore@users.noreply.github.com>
2020-09-08 16:56:25 +0500

    Working on Images.py

---
+++ Practice/images.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+root = Tk()
+root.title("Practicing")
+root.iconbitmap("D:/Haider/PythonGit/PythonPractice/Practice/tnt.ico")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+button_quit = Button(root, text="Exit Program", command=root.quit)
+button_quit.pack()
+
+root.mainloop()
